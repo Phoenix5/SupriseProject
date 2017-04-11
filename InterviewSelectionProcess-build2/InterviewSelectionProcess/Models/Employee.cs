47ea88512569006da6e487d6e22649f781feae11
Phoenix5 <mashelkarprathamesh@gmail.com>
2017-04-11 19:11:33 +0530

    build3-candidate desappears from dropdown when interview set or screened

---
+++ InterviewSelectionProcess-build2/InterviewSelectionProcess/Models/Employee.cs
@@ -0,0 +1,36 @@
+namespace InterviewSelectionProcess.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Employee
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Employee()
+        {
+            InterviewSchedules = new HashSet<InterviewSchedule>();
+        }
+
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string EmailID { get; set; }
+
+      //  public int Count { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<InterviewSchedule> InterviewSchedules { get; set; }
+    }
+}
